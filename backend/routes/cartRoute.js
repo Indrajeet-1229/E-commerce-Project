c9cc7c963bb9a5447715445bb1a151bf20155e72
“Indrajeet1229” <indrajeetbandgar595@gmail.com>
2026-02-18 00:25:45 +0530

    first commit

---
+++ backend/routes/cartRoute.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+import { addToCart, getUseCart, updateCart } from "../controllers/cartController.js";
+import authUser from "../middleware/auth.js";
+
+const cartRouter=express.Router()
+cartRouter.post('/get', authUser, getUseCart)
+cartRouter.post('/add', authUser, addToCart)
+cartRouter.post('/update', authUser, updateCart)
+export default cartRouter
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import connectDb from './config/mongodb.js';
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+
+// app config
+const app=express();
+const port=process.env.PORT || 4000;
+connectDb();
+connectCloudinary();
+
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+
+// api endpoints
+app.use('/api/user',userRouter);
+app.use('/api/product',productRouter);
+app.use('/api/cart',cartRouter);
+app.use('/api/order',orderRouter);
+
+
+app.listen(port, ()=>{
+    console.log("Server is Started On PORT:"+ port)  
+})
